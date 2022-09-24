c2540e82d78a1df75ecc4c98b49702a2ab04ecc8
zubairbinferdous <zubidev71@gmail.com>
2022-09-24 19:29:55 +0600

    ok

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('zubi is back');
+
+function user() {
+  var  A = 30 ;
+    return A ;
+}
+
+function zubi() {
+   var b = 40 ;
+    return b ;
+}
+var one = user();
+var two = zubi();
+
+if(one < two ){
+    console.log('hello world');
+}else{
+    console.log('none');
+}
+
+
+var Name = 'zubair bin ferdous';
+var number = 20 ;
+var booling = true ;
+var array = ['ami' , 'tumi' , 'ola' , 'zubi' ];
