2b821a91a25c13a6f89ae7f47680484fb9c82813
zubairbinferdous <zubidev71@gmail.com>
2022-09-26 01:02:23 +0600

    ok

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-console.log('zubi is back');
+  console.log("zubi is back");
 
-function user() {
-  var  A = 30 ;
-    return A ;
-}
+  function user() {
+    var a = 300;
+    return a;
+  }
 
-function zubi() {
-   var b = 40 ;
-    return b ;
-}
-var one = user();
-var two = zubi();
+  function zubi() {
+    var b = 40;
+    return b;
+  }
+  var one = user();
+  var two = zubi();
 
-if(one < two ){
-    console.log('hello world');
-}else{
-    console.log('none');
-}
+  if (one < two) {
+    console.log("hello world");
+  } else {
+    console.log("nothing is imposibale");
+  }
+
+  var Name = "zubair bin ferdous";
+  var number = 20;
+  var booling = true;
+  var array = ["ami", "tumi", "ola", "zubi"];
 
 
-var Name = 'zubair bin ferdous';
-var number = 20 ;
-var booling = true ;
-var array = ['ami' , 'tumi' , 'ola' , 'zubi' ];
+
+
+
+  function testTwo() {
+    console.log('hello world function two');
+  }
+  console.log(testTwo());
+
+
+  function test() {
+    console.log('hello world function');
+  }
+  test();
+
+  function math(){
+    var a = 20;
+    var b = 30;
+    var c = a + b ;
+    return c;
+  }
+  math();
+  //console.log(math());
+
+
+
+
